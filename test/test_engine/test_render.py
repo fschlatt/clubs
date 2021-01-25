5211b241b13f71d03ee9ab6c3b905237e84a36f4
Ferdinand Schlatt <fschlatt@gmail.com>
2021-01-25 21:03:26 +0100

    clean fixture

--- test/test_engine/test_render.py
+++ test/test_engine/test_render.py
@@ -12,41 +12,45 @@ import numpy as np
 import pytest
 
 import clubs
-from clubs import error
-
-CONFIG = clubs.configs.NO_LIMIT_HOLDEM_SIX_PLAYER
-
-DEALER = clubs.poker.Dealer(**CONFIG)
-
-DEALER._render_config = lambda: {
-    "action": 0,
-    "active": np.array([1, 1]),
-    "all_in": np.array([0, 0]),
-    "community_cards": [clubs.Card("Ac")],
-    "button": 0,
-    "done": 0.0,
-    "hole_cards": [
-        [clubs.Card("Ah"), clubs.Card("Ad")],
-        [clubs.Card("Ks"), clubs.Card("Kd")],
-    ],
-    "pot": 20,
-    "payouts": np.array([0, 0]),
-    "prev_action": None,
-    "street_commits": np.array([0, 0]),
-    "stacks": np.array([200, 200]),
-}
+from clubs import error, render
 
 
-def test_error():
+@pytest.fixture
+def dealer():
+    config = clubs.configs.NO_LIMIT_HOLDEM_SIX_PLAYER
+
+    dealer_ = clubs.poker.Dealer(**config)
+
+    dealer_._render_config = lambda: {
+        "action": 0,
+        "active": np.array([1, 1]),
+        "all_in": np.array([0, 0]),
+        "community_cards": [clubs.Card("Ac")],
+        "button": 0,
+        "done": 0.0,
+        "hole_cards": [
+            [clubs.Card("Ah"), clubs.Card("Ad")],
+            [clubs.Card("Ks"), clubs.Card("Kd")],
+        ],
+        "pot": 20,
+        "payouts": np.array([0, 0]),
+        "prev_action": None,
+        "street_commits": np.array([0, 0]),
+        "stacks": np.array([200, 200]),
+    }
+    return dealer_
+
+
+def test_error(dealer: clubs.Dealer):
     with pytest.raises(error.InvalidRenderModeError):
-        DEALER.render("lala")
+        dealer.render("lala")
 
 
-def test_ascii():
-    DEALER.viewer = None
+def test_ascii(dealer: clubs.Dealer):
+    dealer.viewer = None
     stdout = io.StringIO()
     with redirect_stdout(stdout):
-        DEALER.render("ascii")
+        dealer.render("ascii")
     string = stdout.getvalue()
 
     action_string = "Action on Player 1"
@@ -58,9 +62,11 @@ def test_ascii():
     assert all(sub_string in string for sub_string in sub_strings)
 
 
-def test_human():
-    DEALER.viewer = None
-    DEALER.render()
+def test_human(dealer: clubs.Dealer):
+    dealer.render()
+
+    assert isinstance(dealer.viewer, render.GraphicViewer)
+    port = dealer.viewer.port
 
     start = time.time()
     response: Optional[http.client.HTTPResponse] = None
@@ -68,20 +74,22 @@ def test_human():
         if time.time() - start > 10:
             break
         try:
-            response = urllib.request.urlopen(f"http://localhost:23948")
+            response = urllib.request.urlopen(f"http://localhost:{port}")
             break
         except urllib.error.URLError:
             pass
         time.sleep(1)
 
     assert isinstance(response, http.client.HTTPResponse) and response.status == 200
-    content = response.fp.read().decode().replace("\n", "")
-    svg_xml_string = re.search(r"<\?xml.*?><svg.*</svg>", content).group(0)
+    content = response.read().decode().replace("\n", "")
+    match = re.search(r"<\?xml.*?><svg.*</svg>", content)
+    assert match is not None
+    svg_xml_string = match.group(0)
 
     xml = et.fromstring(svg_xml_string)
     assert len(xml.findall(".//svg[@class='player']")) == 6
 
 
-def test_asciimatics():
-    DEALER.viewer = None
-    DEALER.render("asciimatics")
+def test_asciimatics(dealer: clubs.Dealer):
+    dealer.viewer = None
+    dealer.render("asciimatics")
