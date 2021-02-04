76388f74c69fb5a60bed27526dca75a1a3cfc5ab
Ferdinand Schlatt <fschlatt@gmail.com>
2021-02-04 22:20:33 +0100

    5 sec sleep

--- test/test_render/test_viewer.py
+++ test/test_render/test_viewer.py
@@ -6,5 +6,5 @@ from clubs import render
 def test_base():
     viewer = render.PokerViewer(0, 0, 0)
     start = time.time()
-    viewer.render({}, sleep=1)
+    viewer.render({}, sleep=5)
     assert time.time() - start > 1
