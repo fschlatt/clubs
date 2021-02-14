52ca1473795e902a71a5d914290995d2434493e3
Ferdinand Schlatt <fschlatt@gmail.com>
2021-02-14 15:31:50 +0100

    add doc generation to local tests

--- run_local_tests.sh
+++ run_local_tests.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 isort .
+black .
 flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics
 flake8 . --count --exit-zero --max-complexity=10 --max-line-length=88 --statistics
 mypy . --config-file=mypy.ini
 pytest --cov clubs/ --cov-report html test/
+sphix-apidoc -F -o docs clubs
+cd docs && make html
+cd ..
