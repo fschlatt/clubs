7580fd3cc8d91954d68e8ea5a2c2f96b1bca34d3
Ferdinand Schlatt <fschlatt@gmail.com>
2021-04-06 22:21:14 +0200

    update copyright date

--- clubs/__init__.py
+++ clubs/__init__.py
@@ -1,7 +1,7 @@
 __version__ = "0.1.3"
 __author__ = "Ferdinand Schlatt"
 __license__ = "GPL-3.0"
-__copyright__ = f"Copyright (c) 2020, {__author__}."
+__copyright__ = f"Copyright (c) 2021, {__author__}."
 __homepage__ = "https://github.com/fschlatt/clubs"
 __docs__ = (
     "clubs is a general purpose python poker engine for"
