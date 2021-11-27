b447169e0d8e0513223e4b307400a402a25c0559
zhigal <razvilgrad@gmail.com>
2021-11-28 07:04:43 +1300

    unicode characters variables renamed

--- clubs/poker/card.py
+++ clubs/poker/card.py
@@ -24,12 +24,12 @@ PRETTY_SUITS: Dict[int, str] = {
     8: chr(9827),
 }
 
-CHAR_RANKS = 'A23456789TJCQK'  # https://en.wikipedia.org/wiki/Playing_cards_in_Unicode
-CHARS = {
-    '♠': {k: chr(0x1f0a1 + i) for i, k in enumerate(CHAR_RANKS)},
-    '♥': {k: chr(0x1f0b1 + i) for i, k in enumerate(CHAR_RANKS)},
-    '♦': {k: chr(0x1f0c1 + i) for i, k in enumerate(CHAR_RANKS)},
-    '♣': {k: chr(0x1f0d1 + i) for i, k in enumerate(CHAR_RANKS)},
+UNICODE_RANKS = 'A23456789TJCQK'  # https://en.wikipedia.org/wiki/Playing_cards_in_Unicode
+UNICODE_CHARS = {
+    '♠': {k: chr(0x1f0a1 + i) for i, k in enumerate(UNICODE_RANKS)},
+    '♥': {k: chr(0x1f0b1 + i) for i, k in enumerate(UNICODE_RANKS)},
+    '♦': {k: chr(0x1f0c1 + i) for i, k in enumerate(UNICODE_RANKS)},
+    '♣': {k: chr(0x1f0d1 + i) for i, k in enumerate(UNICODE_RANKS)},
 }
 
 
@@ -105,7 +105,7 @@ class Card:
 
     def __str__(self) -> str:
         if self.unicode:
-            return CHARS[self.suit][self.rank]
+            return UNICODE_CHARS[self.suit][self.rank]
         return f"{self.rank}{self.suit}"
 
     def __repr__(self) -> str:
