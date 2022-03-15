7de6577340d3b7bdad78f6195b3c9e9f258d9ae0
Ferdinand Schlatt <ferdinand.schlatt@informatik.uni-halle.de>
2022-03-15 08:10:34 +0100

    update date

--- clubs/__init__.py
+++ clubs/__init__.py
@@ -1,7 +1,7 @@
 __version__ = "0.1.4"
 __author__ = "Ferdinand Schlatt"
 __license__ = "GPL-3.0"
-__copyright__ = f"2021, {__author__}"
+__copyright__ = f"2022, {__author__}"
 __homepage__ = "https://github.com/fschlatt/clubs"
 __docs__ = (
     "clubs is a general purpose python poker engine for running arbitrary poker "
@@ -12,8 +12,8 @@ try:
     # This variable is injected in the __builtins__ by the build
     # process. It is used to enable importing subpackages when
     # the binaries are not built
-    __CLUBS_SETUP__  # type: ignore
-    __CLUBS_SETUP__ = True
+    __CLUBS_SETUP__
+    __CLUBS_SETUP__: bool = True
 except NameError:
     __CLUBS_SETUP__ = False
 
